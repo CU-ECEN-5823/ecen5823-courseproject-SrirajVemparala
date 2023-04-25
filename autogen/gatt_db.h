9d8a1b99539dee20876ca376cc5a3a2b8f02da3f
SrirajVemparala <srirajvrsp@gmail.com>
2023-04-24 22:29:33 -0600

    master:gatt_services_added

--- autogen/gatt_db.h
+++ autogen/gatt_db.h
@@ -21,7 +21,9 @@ extern const sli_bt_gattdb_t gattdb;
 #define gattdb_measurement_interval           29
 #define gattdb_valid_range                    30
 #define gattdb_button_state                   34
-#define gattdb_ota_control                    38
+#define gattdb_lux_measurement                38
+#define gattdb_IR_Detection                   42
+#define gattdb_ota_control                    46
 
 
 #endif // __GATT_DB_H
